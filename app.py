3a411d130e7cbcfd34a6d74eb8ffa75fe5816da8
HananeJB <jabouhanane@gmail.com>
2022-08-19 11:47:58 +0100

    first commit 1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import mysql.connector
 import pandas as pd
 from scipy import spatial
 import operator
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -48,6 +49,7 @@ from customer
 left join customer_book on customer.id = customer_book.customer_id
 join book on customer_book.book_id = book.id
 GROUP BY customer.id
+
 """)
 books=db.query("""
 select
@@ -81,10 +83,12 @@ user_last_read=db.query("""
 select
 customer_id,
 book_id,
-Max(created_at) as Date 
+created_at
 
 from reading_history
+
 group by customer_id
+order by created_at
 
 """)
 
@@ -141,12 +145,12 @@ def get_user():
 @app.route('/get_similar_books', methods=['GET'])
 
 def get_similar_books_all_read():
-    id=int(request.args.get('id'))
-    param = int(request.args.get('param'))
-    num_books =int(request.args.get('num_books'))
+    id=int(request.args.get('id')) #id of user
+    param = int(request.args.get('param')) # variable of how many similar books to pick by each book in the list
+    num_books =int(request.args.get('num_books')) #how many books do you want to show to the user in total
     all_books = []
     new_user = df_user[df_user['id'] == id].iloc[0].to_frame().T
-
+    #if user never read a book
     if pd.isnull(new_user.books.values[0]) == True:
         book_genre = df_fav[df_fav['customer_id'] == id].iloc[0].to_frame().T
         books = book_genre.genre_id.values[0]
@@ -156,6 +160,7 @@ def get_similar_books_all_read():
         result = [{"id": item[0]} for item in rows]
 
         return json.dumps(result, default=str)
+    #if user already read books
     else:
 
         books = new_user.books.values[0].split(',')
@@ -272,7 +277,6 @@ def get_similar_books_last_read():
         return jsonify({"results": close}), 401
 
 
-
 @app.route('/get_books_by_genre', methods=['GET'])
 
 def get_by_genres():
@@ -287,10 +291,96 @@ def get_by_genres():
 
     return json.dumps(result, default=str)
 
-
 @app.route('/get_books_by_popular', methods=['GET'])
+
 def get_by_popularity():
-    df['rating'].sort_values(by=['rating'], inplace=True)
+    num=int(request.args.get('num'))
+    today = datetime.now()
+    df_latest=df
+    df_latest['created_at'] = pd.to_datetime(df_latest['created_at'])
+    df_latest = df_latest.loc[(df['created_at'].dt.month == today.month)]
+    df_latest = df_latest.sort_values('rating', ascending=False)
+    print(df_latest['id'])
+    result=json.loads(df_latest['id'].head(num).to_json(orient='table',index=False))
+
+    return result
+
+@app.route('/get_similar_books_by_recent', methods=['GET'])
+
+def get_similar_books_by_num_of_recent():
+    id=int(request.args.get('id')) #id of user
+    param = int(request.args.get('param'))
+    num_books =int(request.args.get('num_books'))
+    all_books = []
+    new_user = df_user_last[df_user_last['id'] == id].iloc[0].to_frame().T
+    print(new_user)
+    #if user never read a book
+    if pd.isnull(new_user.books.values[0]) == True:
+        book_genre = df_fav[df_fav['customer_id'] == id].iloc[0].to_frame().T
+        books = book_genre.genre_id.values[0]
+        print(books)
+        query = """select book_id from book_genre WHERE genre_id = '{books}'""".format(books=books)
+        rows = db.query(query)
+        result = [{"id": item[0]} for item in rows]
+
+        return json.dumps(result, default=str)
+    #if user already read books
+
+
+
+    else:
+        if len(new_user.books.values[0])!=1:
+
+            books = new_user.books.values[0].split(',')
+            books = [int(item) for item in books]
+
+            for id in books[0:param]:
+                books = df[df['id'] == id].iloc[0].to_frame().T
+
+                def Similarity(bookId1, bookId2):
+
+                    bookId1 = df[df["id"] == bookId1].index.values[0]
+                    bookId2 = df[df["id"] == bookId2].index.values[0]
+
+                    a = df.iloc[bookId1]
+                    b = df.iloc[bookId2]
+
+                    genreA = a['genres_bin']
+                    genreB = b['genres_bin']
+                    genre = spatial.distance.cosine(genreA, genreB)
+
+                    return genre
+
+                def getNeighbors(baseUser, K):
+                    distances = []
+
+                    for index, user in df.iterrows():
+                        if user['id'] != baseUser['id'].values[0]:
+                            dist = Similarity(baseUser['id'].values[0], user['id'])
+                            distances.append((user['id'], dist))
+
+                    distances.sort(key=operator.itemgetter(1))
+                    neighbors = []
+
+                    for x in range(K):
+                        neighbors.append(distances[x])
+                    return neighbors
+
+                K = len(df)-1
+
+                neighbors = getNeighbors(books, K)
+                close = []
+                for neighbor in neighbors:
+                    if neighbor[1] < 2.5:
+                        close.append(neighbor[0])
+
+                all_books = all_books + close
+                all_books = list(set(all_books))
+                all_books = all_books[0: int(num_books)]
+
+            return jsonify({"results": all_books}), 401
+        else:
+
 
 
 if __name__ == '__main__':
